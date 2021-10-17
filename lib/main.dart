58324b0dd86c877b95e4978a84e9bc1044fb3760
kk-22 <85630292+kk-22@users.noreply.github.com>
2021-10-17 10:09:41 +0900

    アプリ右上のデバッグラベル削除

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'provider demo',
       home: ChangeNotifierProvider<HomeViewModel>(
         create: (_) => HomeViewModel(),
