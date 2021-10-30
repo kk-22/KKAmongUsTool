a24911aea1f28f0761493a5ba38c44bee6fdf31d
kk-22 <85630292+kk-22@users.noreply.github.com>
2021-10-30 19:02:44 +0900

    ラウンド選択の高さを最小限にする

--- lib/view/parts/round_selector.dart
+++ lib/view/parts/round_selector.dart
@@ -20,14 +20,15 @@ class _RoundSelectorState extends State<RoundSelector> {
 
   static const _minHeight = 28.0;
   static const _gridItemHeight = 30.0;
-  static const _gridHeight = _gridItemHeight * 3;
-  static const _maxHeight = _minHeight + _gridHeight;
 
   @override
   Widget build(BuildContext context) {
+    final roundModel = Provider.of<RoundViewModel>(context);
+    final numberOfLine = min(3, ((roundModel.lastRound + 1) ~/ 5 + 1));
+    final double gridHeight = _isExpanding ? _gridItemHeight * numberOfLine : 0;
     return Container(
       color: Colors.white,
-      height: _isExpanding ? _maxHeight : _minHeight,
+      height: _minHeight + gridHeight,
       width: 130,
       child: GestureDetector(
         child: MouseRegion(
@@ -43,21 +44,18 @@ class _RoundSelectorState extends State<RoundSelector> {
                 height: _minHeight,
                 child: FittedBox(
                   fit: BoxFit.fill,
-                  child: Consumer<RoundViewModel>(
-                      builder: (context, model, child) {
-                    return Text(
-                      "ラウンド：${model.currentRound + 1}",
-                      style: TextStyle(
-                        decoration:
-                            _isExpanding ? TextDecoration.underline : null,
-                        color: Colors.blue,
-                      ),
-                    );
-                  }),
+                  child: Text(
+                    "ラウンド：${roundModel.currentRound + 1}",
+                    style: TextStyle(
+                      decoration:
+                          _isExpanding ? TextDecoration.underline : null,
+                      color: Colors.blue,
+                    ),
+                  ),
                 ),
               ),
               if (_isExpanding)
-                SizedBox(height: _gridHeight, child: roundList()),
+                SizedBox(height: gridHeight, child: roundList(roundModel)),
             ],
           ),
         ),
@@ -65,8 +63,7 @@ class _RoundSelectorState extends State<RoundSelector> {
     );
   }
 
-  Widget roundList() {
-    final roundModel = Provider.of<RoundViewModel>(context);
+  Widget roundList(RoundViewModel roundModel) {
     final count = min(roundModel.lastRound + 2, RoundViewModel.maxRound);
     return GridView.count(
       crossAxisCount: RoundViewModel.maxRound ~/ 3,
@@ -79,6 +76,7 @@ class _RoundSelectorState extends State<RoundSelector> {
               "${index + 1}",
               style: TextStyle(
                 color: (isSelected) ? Colors.white : Colors.blue,
+                fontSize: (9 <= index) ? 12 : 14,
               ),
             ),
             style: (isSelected)
